708ae70783880df89b9205d28d72774cfd05437c
adian <adrian.scehura@gmail.com>
2023-09-24 21:35:46 +0000

    add aria-current for current page link

--- src/lib/components/left-sidebar/MenuItem.svelte
+++ src/lib/components/left-sidebar/MenuItem.svelte
@@ -1,14 +1,19 @@
 <script lang="ts">
+	import { page } from '$app/stores';
 	import type { ComponentType } from 'svelte';
 
 	export let icon: ComponentType;
 	export let text: string;
 	export let href: string;
+
+	$: isCurrentPage = $page.url.pathname === href;
 </script>
 
 <a
-	class="ml-3 flex cursor-pointer flex-row gap-6 py-3 font-bold text-subdued duration-300 hover:text-primary"
+	class="ml-3 flex cursor-pointer flex-row gap-6 py-3 font-bold text-subdued duration-300 hover:text-primary
+	{isCurrentPage ? 'text-primary' : ''}"
 	{href}
+	aria-current={isCurrentPage ? 'page' : null}
 >
 	<svelte:component this={icon} class="h-6 w-6" />
 	{text}
